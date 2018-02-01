a2734c16275bbe565fac3b841a8bce02e34208b5
Brad Carlson <b2carlso@yahoo.com>
2018-02-01 20:59:08 +0000

    automatically backed up by learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,10 +1,11 @@
 class ArtistsController < ApplicationController
   def index
-    @artists= Artist.all
+    @artists = Artist.all
   end
 
   def show
-    @artist= Artist.find(params[:id])
+    @artist = Artist.find(params[:id])
+
   end
 
   def new
@@ -49,4 +50,9 @@ class ArtistsController < ApplicationController
   def artist_params
     params.require(:artist).permit(:name)
   end
+
+  def song_count
+    self.songs.count
+  end
+
 end
